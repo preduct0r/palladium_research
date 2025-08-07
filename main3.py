24de60dd4fbfdd1e2a2b172101e3be37375b4cf4
Elena Suhova <sukhovaem@nornik.ru>
2025-08-07 10:00:42 +0000

    Add mapping

--- main3.py
+++ main3.py
@@ -27,82 +27,84 @@ if not dotenv_path:
     raise FileNotFoundError("'.env' file not found. Please create one in the project root.")
 load_dotenv(dotenv_path)
 
-OPENAI_API_KEY = os.getenv("YANDEX_API_KEY")
-folder_id = os.getenv("YANDEX_FOLDER_ID")
-model = ChatOpenAI(base_url="https://llm.api.cloud.yandex.net/v1",  temperature=0.5, model_name=f"gpt://{folder_id}/qwen3-235b-a22b-fp8/latest")
+for article_path in Path(os.getenv("ARTICLE_DIR")).glob("*.pdf"):
+    OPENAI_API_KEY = os.getenv("YANDEX_API_KEY")
+    folder_id = os.getenv("YANDEX_FOLDER_ID")
+    model = ChatOpenAI(base_url="https://llm.api.cloud.yandex.net/v1",  temperature=0.5, model_name=f"gpt://{folder_id}/qwen3-235b-a22b-fp8/latest")
 
-article_path=os.getenv("ARTICLE_PATH")
-article_name = Path(article_path).stem.replace(" ", "_")
+    article_name = Path(article_path).stem.replace(" ", "_")
 
-# ================================
-# Создаем функцию-обертку для нейро запросов
-def get_neuro_with_query(question):
-    """
-    Обертка для get_neuro_response, которая использует get_guery для обработки вопроса
-    """
-    processed_query = get_guery(question, article_name)
-    return get_neuro_response(processed_query)
+    # ================================
+    # Создаем функцию-обертку для нейро запросов
+    def get_neuro_with_query(question):
+        """
+        Обертка для get_neuro_response, которая использует get_guery для обработки вопроса
+        """
+        processed_query = get_guery(question, article_name)
+        return get_neuro_response(processed_query)
 
-# ================================
-# Извлечение информации о статье из OpenAlex
-print("Извлекаем информацию о статье из OpenAlex...")
-get_article_title_info(article_name)
+    # ================================
+    # Извлечение информации о статье из OpenAlex
+    print("Извлекаем информацию о статье из OpenAlex...")
+    get_article_title_info(article_name)
 
-texts, tables = get_article_chunks(article_path)
+    texts, tables = get_article_chunks(article_path)
 
-text_summaries, table_summaries = summarize_article_data(texts, tables)
-article_retriever = get_article_vectorstore(texts, text_summaries, tables, table_summaries)
+    text_summaries, table_summaries = summarize_article_data(texts, tables)
+    article_retriever = get_article_vectorstore(texts, text_summaries, tables, table_summaries)
 
-chain_with_sources = {
-    "context": article_retriever | RunnableLambda(parse_docs),
-    "neuro": RunnableLambda(get_neuro_with_query),
-    "question": RunnablePassthrough(),
-    "article_name": RunnableLambda(lambda _: article_name),
-} | RunnablePassthrough().assign(
-    response=(
-        RunnableLambda(build_prompt3)
-        | model
-        | StrOutputParser()
+    chain_with_sources = {
+        "context": article_retriever | RunnableLambda(parse_docs),
+        "neuro": RunnableLambda(get_neuro_with_query),
+        "question": RunnablePassthrough(),
+        "article_name": RunnableLambda(lambda _: article_name),
+    } | RunnablePassthrough().assign(
+        response=(
+            RunnableLambda(build_prompt3)
+            | model
+            | StrOutputParser()
+        )
     )
-)
 
-# Создаем папку для сохранения ответов
-answers_dir = Path("data") / article_name / "answers"
-answers_dir.mkdir(parents=True, exist_ok=True)
+    # Создаем папку для сохранения ответов
+    answers_dir = Path("data") / article_name / "answers"
+    answers_dir.mkdir(parents=True, exist_ok=True)
 
-questions_and_files = [   
-    ("Напиши одним предложением о какой промышленной технологии идет речь в этой статье. Выведи только название технологии, ничего больше, ответ должен содержать от 4 до 15 слов", "technology.txt"),
-    ("Какова основная идея изложенна в статье?", "idea.txt"),
-    ("Какое направление, тематика у этой статьи? Выведи только тематики ничего больше. Например: 'Катализ, палладий, деароматизация, нефтехимия, каталитическая переработка'", "tematic.txt"),
-    ("Какое потенциальное потребление палладия при применении подхода из статьи в кг? Если в статье нет информации, попробуй поразмышлять на основе знаний, которые у тебя есть. Выведи только вывод, ничего больше", "potential_consumption.txt"),
-    ("Какой уровень развития подхода из статьи?", "technology_development_level.txt"),
-    ("Какова новизна применения палладия при применении подхода из статьи?", "palladium_novelty.txt"),
-    ("Какова научно-техническая реализуемость внедрения палладия при применении подхода из статьи?", "technical_feasibility.txt"),
-    ("Какой коммерческий потенциал внедрения палладия при применении подхода из статьи?", "commercial_potential.txt"),
-    ("Какие конкурентные преимущества палладия в подходе из статьи?", "competitive_advantages.txt"),
-    ("Какой уровень готовности подхода из статьи с палладием?", "technology_readiness_level.txt"),
-    ("Какая перспективность рынка разработки при применении подхода из статьи?", "market_prospects.txt"),
-    ("Какой уровень рыночного коммерческого потенциала при применении подхода из статьи?", "market_commercial_potential.txt"),
-    ("Какова сложность разработки при применении подхода из статьи?", "development_complexity.txt"),
-    ("Какова сложность внедрения при применении подхода из статьи?", "implementation_complexity.txt"),
-    ("Каков потенциал коммерциализации при применении подхода из статьи?", "commercialization_potential.txt"),
-    ("Какова предполагаемая длительность разработки при применении подхода из статьи? ", "development_duration.txt"),
-]
+    questions_and_files = [   
+        ("Напиши одним предложением о какой промышленной технологии идет речь в этой статье. Выведи только название технологии, ничего больше, ответ должен содержать от 4 до 15 слов", "technology.txt"),
+        ("Какова основная идея изложенна в статье?", "idea.txt"),
+        ("Какое направление, тематика у этой статьи? Выведи только тематики ничего больше. Например: 'Катализ, палладий, деароматизация, нефтехимия, каталитическая переработка'", "tematic.txt"),
+        ("Какой тип у этого проекта? Например: 'Прикладной среднесрочный'", "type.txt"),
+        ("Какое потенциальное потребление палладия при применении подхода из статьи в кг?", "potential_consumption.txt"),
+        ("Какой уровень развития подхода из статьи?", "technology_development_level.txt"),
+        ("Какова новизна применения палладия при применении подхода из статьи?", "palladium_novelty.txt"),
+        ("Какова научно-техническая реализуемость внедрения палладия при применении подхода из статьи?", "technical_feasibility.txt"),
+        ("Какой коммерческий потенциал внедрения палладия при применении подхода из статьи?", "commercial_potential.txt"),
+        ("Какие конкурентные преимущества палладия в подходе из статьи?", "competitive_advantages.txt"),
+        ("Какой уровень готовности подхода из статьи с палладием?", "technology_readiness_level.txt"),
+        ("Какая перспективность рынка разработки при применении подхода из статьи?", "market_prospects.txt"),
+        ("Какой уровень рыночного коммерческого потенциала при применении подхода из статьи?", "market_commercial_potential.txt"),
+        ("Какова сложность разработки при применении подхода из статьи?", "development_complexity.txt"),
+        ("Какова сложность внедрения при применении подхода из статьи?", "implementation_complexity.txt"),
+        ("Какова предполагаемая длительность разработки при применении подхода из статьи? ", "development_duration.txt"),
+        ("Стоит ли взять в проработку подход из статьи?", "decision.txt"),
+        ("Оставь свои комментарии по поводу подхода из статьи? Например:Необходима оценка себестоимости катализатора, промотированного палладием по отношению к непромотированному катализатору в сравнении с приобретаемыми положительными качествами (конверсия, селективность, срок службы)", "comments.txt"),
+    ]
 
-for question, filename in questions_and_files:
-    response = chain_with_sources.invoke(question)
+    for question, filename in questions_and_files:
+        response = chain_with_sources.invoke(question)
 
-    print("Response:", response['response'])
-    
-    # Сохраняем ответ в файл
-    file_path = answers_dir / filename
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.write(response['response'])
-    print(f"Ответ сохранен в: {file_path}")
+        print("Response:", response['response'])
+        
+        # Сохраняем ответ в файл
+        file_path = answers_dir / filename
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(response['response'])
+        print(f"Ответ сохранен в: {file_path}")
 
-    print("\n\nContext:")
-    for text in response['context']['texts']:
-        print(text.text)
-        print("Page number: ", text.metadata.page_number)
-        print("\n" + "-"*50 + "\n")
+        print("\n\nContext:")
+        for text in response['context']['texts']:
+            print(text.text)
+            print("Page number: ", text.metadata.page_number)
+            print("\n" + "-"*50 + "\n")
 
---
+++ write_report.py
@@ -0,0 +1,28 @@
+
+
+map_names = {
+    "comments": "Комментарии",
+    "decision": "Принятие решения",
+
+    "commercial_potential": "Коммерческий потенциал внедрения Pd",
+    "competitive_advantages": "Конкурентные преимущества Pd",
+    "date": "Дата публикации",
+    "development_complexity": "Сложность разработки",
+    "development_duration": "Длительность разработки",
+    "doi": "Ссылка [DOI]",
+    "idea": "Идея",
+    "implementation_complexity": "Сложность внедрения",
+    "journal": "Журнал",
+    "market_commercial_potential": "Уровень рыночного потенциала (коммерция)",
+    "market_prospects": "Перспективность рынка (разработка)",
+    "potential_consumption": "Потенциальное потребление Pd, кг ",
+    "palladium_novelty": "Новизна применения Pd",
+    "technical_feasibility": "Научно-техническая реализуемость внедрения Pd",
+    "technology_development_level": "Уровень развития технологии под которую делается продукт",
+    "technology_readiness_level": "Уровень готовности технологии с Pd",
+    "technology": "Промышленная технология",   
+    "type": "Тип проекта",
+    "tematic": "Направление (тематика)",
+}
+
+
